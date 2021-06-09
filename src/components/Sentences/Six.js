729f05d337a7820b9f5cbd33eef64cb46cef1651
Andrew Alvarado <andrew.jose.av@gmail.com>
2021-06-09 00:45:24 -0600

    Six basic logic added

--- src/components/Sentences/Six.js
+++ src/components/Sentences/Six.js
@@ -2,10 +2,51 @@ import React, { useState, useEffect } from 'react';
 
 
 const Six = () => {
+    const [A, setA] = useState("");
+    const [B, setB] = useState("");
+
+    const handleAChange = (event) => setA(event.target.value);
+    const handleBChange = (event) => setB(event.target.value);
+
+    const swap = () => {
+        //Destructurin assigment does not work on hooks this way
+        // [A, B] = [B, A]
+        let C = A;
+        setA(B);
+        setB(C);
+    }
+
 
     return (
         <React.Fragment>
-            <h1>6</h1>
+            <h1>6. Dadas dos variables A y B realizar un intercambio entre ambas.</h1>
+            <div>
+                <label>A: </label>
+                <input
+                    type="text"
+                    value={A}
+                    placeholder="Enter A value."
+                    onChange={handleAChange}
+                />
+            </div>
+            <div>
+                <label>B: </label>
+                <input
+                    type="text"
+                    value={B}
+                    placeholder="Enter B value."
+                    onChange={handleBChange}
+                />
+            </div>
+            <div>
+                <label>Variables: </label>
+                <h1>A={A}  B={B}</h1>
+            </div>
+
+            <button onClick={swap}>
+                Swap
+            </button>
+
         </React.Fragment>
     );
 }
