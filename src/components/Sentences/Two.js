024b57300f7596cebae980bfeb6b1fc62133fca7
Andrew Alvarado <andrew.jose.av@gmail.com>
2021-06-08 21:45:50 -0600

    Add basic logic to Two

--- src/components/Sentences/Two.js
+++ src/components/Sentences/Two.js
@@ -2,10 +2,66 @@ import React, { useState, useEffect } from 'react';
 
 
 const Two = () => {
+    const [first, setFirst] = useState("");
+    const [second, setSecond] = useState("");
+    const [result, setResult] = useState("");
+
+    const handleFirstChange = (event) => setFirst(event.target.value);
+    const handleSecondChange = (event) => setSecond(event.target.value);
+
+    const add = () => {
+        setResult(+first + +second);
+    }
+    const sub = () => {
+        setResult(first - second);
+    }
+    const mul = () => {
+        setResult(first * second);
+    }
+    const div = () => {
+        setResult(first / second);
+    }
+
 
     return (
         <React.Fragment>
-            <h1>2</h1>
+            <h1>2. Sumar, restar, multiplicar o dividir numeros enteros.</h1>
+            <div>
+                <label>First Number: </label>
+                <input
+                    type="number"
+                    value={first}
+                    placeholder="Enter first number"
+                    onChange={handleFirstChange}
+                />
+            </div>
+
+            <div>
+                <label>Second Number: </label>
+                <input
+                    type="number"
+                    value={second}
+                    placeholder="Enter second number"
+                    onChange={handleSecondChange}
+                />
+            </div>
+            <div>
+                <button onClick={add}>
+                    Add
+                </button>
+                <button onClick={sub}>
+                    Substract
+                </button>
+                <button onClick={mul}>
+                    Multiply
+                </button>
+                <button onClick={div}>
+                    Divide
+                </button>
+            </div>
+            <div>
+                <h1>Result: {result}</h1>
+            </div>
         </React.Fragment>
     );
 }
