2ca81337863545118c18baca5194f22944a9943c
Andrew Alvarado <andrew.jose.av@gmail.com>
2021-06-08 21:52:56 -0600

    Basic logic to Four added

--- src/components/Sentences/Four.js
+++ src/components/Sentences/Four.js
@@ -2,10 +2,49 @@ import React, { useState, useEffect } from 'react';
 
 
 const Four = () => {
+    const [base, setBase] = useState("");
+    const [height, setHeight] = useState("");
+    const [result, setResult] = useState("");
+
+    const handleBaseChange = (event) => setBase(event.target.value);
+    const handleHeightChange = (event) => setHeight(event.target.value);
+
+    const calculate = () => {
+        setResult((base*height)/2);
+    }
 
     return (
         <React.Fragment>
-            <h1>4</h1>
+            <h1>4. Calcular la superficie de un triangulo.</h1>
+            <div>
+                <label>First Number: </label>
+                <input
+                    type="number"
+                    value={base}
+                    placeholder="Enter triangle base"
+                    onChange={handleBaseChange}
+                />
+            </div>
+
+            <div>
+                <label>Second Number: </label>
+                <input
+                    type="number"
+                    value={height}
+                    placeholder="Enter triangle height."
+                    onChange={handleHeightChange}
+                />
+            </div>
+            <div>
+                <button onClick={calculate}>
+                    Calculate area.
+                </button>
+            </div>
+            <div>
+                <h1>Result: {result}</h1>
+            </div>
+
+
         </React.Fragment>
     );
 }
