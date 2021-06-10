d75767581589eac2cbf99c923bc776c8363619f8
Andrew Alvarado <andrew.jose.av@gmail.com>
2021-06-09 18:30:18 -0600

    Eight and Ten basic logic

--- src/components/Sentences/Eight.js
+++ src/components/Sentences/Eight.js
@@ -2,10 +2,51 @@ import React, { useState, useEffect } from 'react';
 
 
 const Eight = () => {
+    const [firstGrade, setFirstGrade] = useState("");
+    const [secondGrade, setSecondGrade] = useState("");
+    const [thirdGrade, setThirdGrade] = useState("");
+
+    const handleFirstGradeChange = (event) => setFirstGrade(event.target.value);
+    const handleSecondGradeChange = (event) => setSecondGrade(event.target.value);
+    const handleThirdGradeChange = (event) => setThirdGrade(event.target.value);
+
+
 
     return (
         <React.Fragment>
-            <h1>8</h1>
+            <h1>8. Obtener el promedio simple de un estudiente con sus tres notas parciales.</h1>
+            <div>
+                <label>First grade: </label>
+                <input
+                    type="number"
+                    value={firstGrade}
+                    placeholder="Enter the first grade."
+                    onChange={handleFirstGradeChange}
+                />
+            </div>
+            <div>
+                <label>Second grade: </label>
+                <input
+                    type="number"
+                    value={secondGrade}
+                    placeholder="Enter the second grade."
+                    onChange={handleSecondGradeChange}
+                />
+            </div>
+            <div>
+                <label>Third grade: </label>
+                <input
+                    type="number"
+                    value={thirdGrade}
+                    placeholder="Enter the third grade."
+                    onChange={handleThirdGradeChange}
+                />
+            </div>
+
+            <div>
+                <h1>Average: {(+firstGrade + +secondGrade + +thirdGrade) / 3}</h1>
+            </div>
+
         </React.Fragment>
     );
 }
--- src/components/Sentences/Ten.js
+++ src/components/Sentences/Ten.js
@@ -2,10 +2,40 @@ import React, { useState, useEffect } from 'react';
 
 
 const Ten = () => {
+    const CDSize = 700;
+    const megabyte = 1024;
+    const [hardDrive, setHardDrive] = useState();
+
+
+    const handleHardDriveChange = (event) => setHardDrive(event.target.value);
+
+
+    const calculateCD = () => {
+        const HDDInBytes = hardDrive * megabyte;
+        const necesaryCDs = HDDInBytes / CDSize;
+
+        return (`Space needed: ${HDDInBytes} bytes. \n
+                Required CDs for the backup: ${Math.ceil(necesaryCDs)}`)
+
+    }
+
+
 
     return (
         <React.Fragment>
-            <h1>10</h1>
+            <h1>10. Calcular el número de CDs necesarios para hacer un respaldo de un disco duro lleno.</h1>
+            <div>
+                <label>Total hard drive space in GB: </label>
+                <input
+                    type="number"
+                    value={hardDrive}
+                    placeholder="Enter the total hard drive space in GB."
+                    onChange={handleHardDriveChange}
+                />
+            </div>
+            <div>
+                <h1>{calculateCD()}</h1>
+            </div>
         </React.Fragment>
     );
 }
